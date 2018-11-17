f70cdd4e3863af56b6b2f062119524af3468306a
Frol <artem.mihailovich@gmail.com>
2018-11-18 01:05:12 +0400

    Змейка жрет

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -27,17 +27,29 @@ namespace Snake
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
 
-            while(true)
-            {
-                if(Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HandleKey(key.Key);
-                   
-                }
-                Thread.Sleep(100);
-                snake.Move();
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Drow();
 
+            while (true)
+            {
+                if(snake.Eat( food ) )
+				{
+					food = foodCreator.CreateFood();
+					food.Drow();
+				}
+				else
+				{
+					snake.Move();
+				}					
+
+				Thread.Sleep( 100 );
+
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey( key.Key );
+				}
             }
 
             
